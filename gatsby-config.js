9db514aeb9d3e6b4e1a2b4fcbc5a3e38e3f9ed06
“Daniel <danielwdewald@gmail.com>
2024-03-04 15:30:42 -0500

    Integrate Sanity CMS for section descriptions

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "aynbd1l4",
+        dataset: "production",
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
